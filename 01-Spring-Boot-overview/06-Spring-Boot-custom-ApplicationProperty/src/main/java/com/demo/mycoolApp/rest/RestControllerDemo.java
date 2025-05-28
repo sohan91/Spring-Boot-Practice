b9c38cde941f1607a5876ee625a7fd3fe0d33ac4
sohan <sohanprasad2003@gmail.com>
2025-05-28 17:04:52 +0530

    custom property for web

---
+++ 01-Spring-Boot-overview/06-Spring-Boot-custom-ApplicationProperty/src/main/java/com/demo/mycoolApp/rest/RestControllerDemo.java
@@ -0,0 +1,29 @@
+package com.demo.mycoolApp.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestControllerDemo {
+        @GetMapping("/")
+                public String toString()
+        {
+            return "Hello World";
+        }
+        @GetMapping("/workOut")
+               public String getWorkOut()
+        {
+            return "It's working on Workout!!";
+        }
+        @GetMapping("/fortune")
+        public String fortune()
+        {
+            return "Today is Memorable Dayy!!";
+        }
+
+
+
+    }
+
