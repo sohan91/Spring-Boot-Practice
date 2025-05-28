5dde82fec6e0ecd3a5bb412a07dea33f9a31f7e2
sohan <sohanprasad2003@gmail.com>
2025-05-28 20:55:47 +0530

    adding the additional code in Controller

--- 01-Spring-Boot-overview/06-Spring-Boot-custom-ApplicationProperty/src/main/java/com/demo/mycoolApp/rest/RestControllerDemo.java
+++ 01-Spring-Boot-overview/06-Spring-Boot-custom-ApplicationProperty/src/main/java/com/demo/mycoolApp/rest/RestControllerDemo.java
@@ -1,5 +1,6 @@
 package com.demo.mycoolApp.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,8 +23,6 @@ public class RestControllerDemo {
         {
             return "Today is Memorable Dayy!!";
         }
-
-
 
     }
 
