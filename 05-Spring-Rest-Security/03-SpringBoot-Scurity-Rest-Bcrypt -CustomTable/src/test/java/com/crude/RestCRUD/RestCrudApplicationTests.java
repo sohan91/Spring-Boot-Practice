03652a0fbe0d4e9892a8f8af8807cd7e36c6232b
sohan <sohanprasad2003@gmail.com>
2025-07-09 21:12:22 +0530

    create a custom table for spring security with Bcrypt

---
+++ 05-Spring-Rest-Security/03-SpringBoot-Scurity-Rest-Bcrypt -CustomTable/src/test/java/com/crude/RestCRUD/RestCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crude.RestCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
