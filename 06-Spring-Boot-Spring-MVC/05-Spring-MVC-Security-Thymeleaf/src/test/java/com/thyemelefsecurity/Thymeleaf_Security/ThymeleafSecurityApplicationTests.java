53017ab67253557eef0c5f012063fb569cc11884
sohan <sohanprasad2003@gmail.com>
2025-07-14 21:38:43 +0530

    add validation and authentication

---
+++ 06-Spring-Boot-Spring-MVC/05-Spring-MVC-Security-Thymeleaf/src/test/java/com/thyemelefsecurity/Thymeleaf_Security/ThymeleafSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thyemelefsecurity.Thymeleaf_Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
