556e9c765ebc369e67b81e18330e5eac9b53ab00
sohan <sohanprasad2003@gmail.com>
2025-07-14 21:42:02 +0530

    check authentication with corresponding roles

--- 06-Spring-Boot-Spring-MVC/05-Spring-MVC-Security-Thymeleaf/src/main/java/com/thyemelefsecurity/Thymeleaf_Security/controller/DemoController.java
+++ 06-Spring-Boot-Spring-MVC/05-Spring-MVC-Security-Thymeleaf/src/main/java/com/thyemelefsecurity/Thymeleaf_Security/controller/DemoController.java
@@ -1,2 +1,26 @@
-package com.thyemelefsecurity.Thymeleaf_Security.controller;public class DemoController {
+package com.thyemelefsecurity.Thymeleaf_Security.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/home")
+    public String home()
+    {
+        return "home";
+    }
+    @GetMapping("/showLoginPage")
+    public String form()
+    {
+          return "login-form";
+    }
+//    @PostMapping("/authenticateUser")
+//    public String authenticate()
+//    {
+//        return "authenticate-user";
+//    }
 }
--- 06-Spring-Boot-Spring-MVC/05-Spring-MVC-Security-Thymeleaf/src/main/java/com/thyemelefsecurity/Thymeleaf_Security/securityconfig/SecurityConfiguration.java
+++ 06-Spring-Boot-Spring-MVC/05-Spring-MVC-Security-Thymeleaf/src/main/java/com/thyemelefsecurity/Thymeleaf_Security/securityconfig/SecurityConfiguration.java
@@ -1,2 +1,40 @@
-package com.thyemelefsecurity.Thymeleaf_Security.securityconfig;public class SecurityConfiguration {
+package com.thyemelefsecurity.Thymeleaf_Security.securityconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public InMemoryUserDetailsManager detailsManager()
+    {
+        UserDetails sohan = User.builder().username("sohan")
+                .password("{noop}test123").roles("EMPLOYEE").build();
+        UserDetails raju = User.builder().username("raju")
+                .password("{noop}test123").roles("EMPLOYEE","MANAGER").build();
+        UserDetails vikas = User.builder().username("vikas")
+                .password("{noop}test123").roles("EMPLOYEE","MANAGER","ADMIN").build();
+        return new InMemoryUserDetailsManager(sohan,raju,vikas);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity security) throws Exception
+    {
+        security.authorizeHttpRequests(configure->configure.anyRequest().authenticated()
+                )
+                .formLogin(form->form.loginPage("/showLoginPage")
+                .loginProcessingUrl("/authenticateUser")
+                        .defaultSuccessUrl("/home", true)
+                        .permitAll()
+                )
+                        .logout(LogoutConfigurer::permitAll);
+        return security.build();
+    }
 }
