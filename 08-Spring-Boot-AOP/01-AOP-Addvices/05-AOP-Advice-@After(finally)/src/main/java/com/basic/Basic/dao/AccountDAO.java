5b4236526ec193060d10e945d2c5875f491493d3
sohan <sohanprasad2003@gmail.com>
2025-07-22 22:01:37 +0530

    add @After advice

---
+++ 08-Spring-Boot-AOP/01-AOP-Addvices/05-AOP-Advice-@After(finally)/src/main/java/com/basic/Basic/dao/AccountDAO.java
@@ -0,0 +1,13 @@
+package com.basic.Basic.dao;
+
+import java.util.List;
+
+public interface AccountDAO {
+   public void addAccount();
+   public void setName(String name);
+   public String getName();
+   void accountHolders(List<String> holders);
+   List<String> returnHolders();
+   void setAge(int age);
+   int getAge();
+}
